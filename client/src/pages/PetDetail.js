e2373af1d516eba381d1f2be3842108d5f63637b
Nadine <nadine_paez@hotmail.com>
2022-02-02 12:37:04 -0500

    fixed image

--- client/src/pages/PetDetail.js
+++ client/src/pages/PetDetail.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_PET } from '../utils/queries';
-import Auth from '../utils/auth';
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_PET } from "../utils/queries";
+import Auth from "../utils/auth";
+import CommentList from "../components/CommentList";
+import CommentForm from "../components/CommentForm";
 // import '../../assets/css/index.css';
 
-const PetDetail = props => {
+const PetDetail = (props) => {
   const { petId } = useParams();
 
   const { loading, data } = useQuery(QUERY_PET, {
-    variables: { id: petId }
+    variables: { id: petId },
   });
 
   const pet = data?.pet || {};
@@ -21,49 +21,44 @@ const PetDetail = props => {
   }
 
   return (
-
     <div>
-
       <div className="media-content">
-                      <p className="title is-4">{pet.name}</p>
-                      <p className="subtitle is-6">{pet.petBreed}</p>
-                    </div>
+        <p className="title is-4">{pet.name}</p>
+        <p className="subtitle is-6">{pet.petBreed}</p>
+      </div>
+      <div className="column is-half">
+        <div className="card">
+          <div className="card-image">
+            <div className="media-left">
+              <figure className="image is-4by3">
+                <img src={pet.image} alt="image of a dog"></img>
+              </figure>
+            </div>
+          </div>
+        </div>
 
-              {/* <div className="card-content">
-                  <div className="media">
-                    <div className="media-left">
-                      <figure className="image is-48x48">
-                        <img
-                          src={require("../../assets/images/logo.png")}
-                          alt="Placeholder image"
-                        ></img>
-                      </figure>
-                    </div>
-                  </div>
-               </div> */}
-
-      <div className="card mb-3">
-        {/* <p className="card-header">
+        <div className="card-content">
+          {/* <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {pet.username}
           </span>{' '}
           Pet Detail {new Date(pet.createdAt).toLocaleString}
         </p> */}
-         {/* <div className="media-content">
+          {/* <div className="media-content">
            <p className="title is-4">{pet.name}</p>
            <p className="subtitle is-6">{pet.petBreed}</p>
           </div> */}
-        <ul>
-        <li>Name: {pet.name}</li>
-        <li>Breed: {pet.petBreed}</li>
-        <li>Personality: {pet.petPersonality}</li>
-        <li>Pet Preferences: {pet.petPreference}</li>
-      </ul>
-        <div className="card-body">
-          <p>{pet.petText}</p>
-        </div>
-        <form class="edit-post-form">
-    {/* <div>
+          <ul>
+            <li>Name: {pet.name}</li>
+            <li>Breed: {pet.petBreed}</li>
+            <li>Personality: {pet.petPersonality}</li>
+            <li>Pet Preferences: {pet.petPreference}</li>
+          </ul>
+          <div className="card-body">
+            <p>{pet.petText}</p>
+          </div>
+          <form class="edit-post-form">
+            {/* <div>
       <input
         name="edit-pet"
         class="input is-link"
@@ -71,15 +66,25 @@ const PetDetail = props => {
         value="{{post.petDetail}}"
       />
     </div> */}
-    <div class="is-flex is-justify-content-space-between">
-      <button class="button is-primary" type="submit">Edit Pet</button>
-      <button id="delete-post-btn" class="button is-danger" type="button">Delete Pet</button>
-    </div>
-    </form>
+            <div class="is-flex is-justify-content-space-between">
+              <button class="button is-primary" type="submit">
+                Edit Pet
+              </button>
+              <button
+                id="delete-post-btn"
+                class="button is-danger"
+                type="button"
+              >
+                Delete Pet
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="card-content">
+          {pet.commentCount > 0 && <CommentList comments={pet.comments} />}
+          {Auth.loggedIn() && <CommentForm petId={pet._id} />}
+        </div>
       </div>
-
-      {pet.commentCount > 0 && <CommentList comments={pet.comments} />}
-      {Auth.loggedIn() && <CommentForm petId={pet._id} />}
     </div>
   );
 };
