1cfcbb6756e00524913d8306af0eff8169250768
akidane23 <arsemakidan@gmail.com>
2022-02-02 11:35:42 -0500

    added delete and edit buttons

--- client/src/pages/PetDetail.js
+++ client/src/pages/PetDetail.js
@@ -5,6 +5,7 @@ import { QUERY_PET } from '../utils/queries';
 import Auth from '../utils/auth';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
+// import '../../assets/css/index.css';
 
 const PetDetail = props => {
   const { petId } = useParams();
@@ -28,23 +29,53 @@ const PetDetail = props => {
                       <p className="subtitle is-6">{pet.petBreed}</p>
                     </div>
 
-      <ul>
+              {/* <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-48x48">
+                        <img
+                          src={require("../../assets/images/logo.png")}
+                          alt="Placeholder image"
+                        ></img>
+                      </figure>
+                    </div>
+                  </div>
+               </div> */}
+
+      <div className="card mb-3">
+        {/* <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {pet.username}
+          </span>{' '}
+          Pet Detail {new Date(pet.createdAt).toLocaleString}
+        </p> */}
+         {/* <div className="media-content">
+           <p className="title is-4">{pet.name}</p>
+           <p className="subtitle is-6">{pet.petBreed}</p>
+          </div> */}
+        <ul>
         <li>Name: {pet.name}</li>
         <li>Breed: {pet.petBreed}</li>
         <li>Personality: {pet.petPersonality}</li>
         <li>Pet Preferences: {pet.petPreference}</li>
       </ul>
-
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            {pet.username}
-          </span>{' '}
-          comment on {new Date(pet.createdAt).toLocaleString}
-        </p>
         <div className="card-body">
           <p>{pet.petText}</p>
         </div>
+        <form class="edit-post-form">
+    {/* <div>
+      <input
+        name="edit-pet"
+        class="input is-link"
+        type="text"
+        value="{{post.petDetail}}"
+      />
+    </div> */}
+    <div class="is-flex is-justify-content-space-between">
+      <button class="button is-primary" type="submit">Edit Pet</button>
+      <button id="delete-post-btn" class="button is-danger" type="button">Delete Pet</button>
+    </div>
+    </form>
       </div>
 
       {pet.commentCount > 0 && <CommentList comments={pet.comments} />}
