617a2cb8774983c0ca387227db0509d9b76a4b2b
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2022-02-02 12:31:37 -0500

    styling updates on comment form

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const CommentList = ({ comments }) => {
   return (
-    <div className="card mb-3">
-      <div className="card-header">
-        <span className="text-light">Comments</span>
-      </div>
-      <div className="card-body">
+    <div className="section">
+      
+        <span className="subtitle is-4 text-light">Comments</span>
+    
+      <div className="box">
         {comments &&
           comments.map(comment => (
             <p className="pill mb-3" key={comment._id}>
-              {comment.commentBody} //{' '}
-              <Link to={`/profile/${comment.username}`} style={{ fontWeight: 700 }}>
-                {comment.username} on {comment.createdAt}
-              </Link>
+              {comment.commentBody} {' '}
+              <a>
+                By {comment.username}
+              </a>
             </p>
           ))}
       </div>
