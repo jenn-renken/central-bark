2344114b76bc5aab986af3adfaade2bb7828f0a9
Nadine <nadine_paez@hotmail.com>
2022-02-02 11:51:40 -0500

    clean up

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -33,8 +33,6 @@ const petSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  //add in when ready for comment!!!
-  //don't forget to create commentSchema
   //if time permits add "picture like" feature as well (virtual like count)
     comments: [commentSchema]
   },
@@ -53,10 +51,3 @@ const Pet = model('Pet', petSchema);
 
 module.exports = Pet;
 
-// in Pet.js, update to a model instead of just a schema, add a userId field with the proper datatype for a mongo object id;
-// in addPet,
-  // first create a new Pet object using the args for the pet info, and context.user._id for the userId field.
-  // get the _id field from the newly returned pet, and push it to the savedPets property of the relevant user.
-// in removePet,
-  // first pull the relevant id from the user's savedPets property.
-  // delete the Pet object.
